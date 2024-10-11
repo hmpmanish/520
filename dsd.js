50dfa0e2d1140d54a43993211e941d9abf3ddcde
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2024-10-11 20:25:21 +0530

    Update dsd.js

--- dsd.js
+++ dsd.js
@@ -1,5 +1,1 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("formResponse").innerText = "Thank you for your message! We will get back to you shortly.";
-    document.getElementById("contactForm").reset();
-});
+
